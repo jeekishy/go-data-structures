4912643d7d9a7d0b3a9319ccd51114ff693401f8
Kish Jeetun <jeetun@gmail.com>
2022-11-15 08:01:07 +1000

    add delete method

--- hastTable/main.go
+++ hastTable/main.go
@@ -7,7 +7,7 @@ import (
 
 // implement a simple custom hash table
 
-// HastTable struc
+// HastTable struct
 type HastTable struct {
 	size int
 	data map[int]int
@@ -15,13 +15,20 @@ type HastTable struct {
 
 func main() {
 	ht := NewHashTable()
-	ht.set("apple", 10000)
-	ht.set("pineapple", 20000)
-	ht.set("smith", 30000)
 
-	fmt.Println(ht.get("apple"))
-	fmt.Println(ht.get("pineapple"))
-	fmt.Println(ht.get("smith"))
+	// adding to a hash table
+	ht.set("apple", 10000)     // O(1)
+	ht.set("pineapple", 20000) // O(1)
+	ht.set("smith", 30000)     // O(1)
+
+	// getting value from hash table
+	fmt.Println(ht.get("apple"))     // O(1)
+	fmt.Println(ht.get("pineapple")) // O(1)
+	fmt.Println(ht.get("smith"))     // O(1)
+
+	// deleting value from hash table
+	ht.delete("pineapple")           // O(1)
+	fmt.Println(ht.get("pineapple")) // O(1)
 }
 
 // NewHashTable get new hashTable
@@ -43,7 +50,17 @@ func (h *HastTable) get(key string) int {
 	return h.data[hash(key)]
 }
 
-// hash generates a int from provide key string
+// delete removes data from hast table
+func (h *HastTable) delete(key string) bool {
+	// check map has key
+	if _, ok := h.data[hash(key)]; ok {
+		delete(h.data, hash(key))
+		return true
+	}
+	return false
+}
+
+// hash generates an int from provide key string
 func hash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
